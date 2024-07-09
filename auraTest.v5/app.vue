fe372de1059535c029cac416bea887b71af0bedf
Catarina Roseta <catarinaroseta.dev@gmail.com>
2024-07-09 22:48:19 +0100

    Basic setup

--- auraTest.v5/app.vue
+++ auraTest.v5/app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <Button label="Save" icon="pi pi-check" iconPos="right" />  </div>
+    <Card>
+      <template #title>
+        <p>Lorem iposum</p>
+      </template>
+    </Card>
 </template>
